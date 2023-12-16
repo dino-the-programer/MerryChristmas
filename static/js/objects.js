5df8f0cb5010f785232641d4299f0d0b34c49994
dino-the-programer <dinop0928@gmail.com>
2023-12-17 00:34:04 +0530

    adding trees to the world

--- static/js/objects.js
+++ static/js/objects.js
@@ -3,11 +3,11 @@ class World{
         this.raduis = (screenWidth / 2) * (screenHeight > screenWidth ? (screenHeight / screenWidth) : 1 / (screenHeight / screenWidth));
         this.pos = {
             x: screenWidth / 2,
-            y: screenHeight + (this.raduis - screenHeight / 2),
+            y: screenHeight + (this.raduis - screenHeight*0.30),
         };
         this.style = style;
         this.props = props;
-        this.angle = 0;
+        this.angle = Math.PI;
     }
 
     draw(ctx) {
@@ -17,12 +17,49 @@ class World{
         ctx.fillStyle = this.style;
         ctx.beginPath();
         ctx.arc(0,0, this.raduis, 0, Math.PI * 2);
-        ctx.fillRect(0, -this.raduis, 100, 100);
         ctx.fill();
+        this.props[0].array.forEach(tree => {
+            drawTree(ctx, this.raduis, tree.angle, tree.height,"#234f42","#060f1f",4);
+        });
         ctx.restore();
     }
 
     rotate(angle) {
-        this.angle = angle;
+        this.angle += angle;
     }
+}
+class treeCollection{
+    constructor() {
+        this.array = [];
+    }
+    add(angle, height) {
+        this.array.push({ angle: angle, height: height });
+    }
+}
+
+
+function drawTree(ctx, raduis, angle, height,leafcol,trunkcol,stemThickness) {
+    ctx.beginPath();
+    ctx.strokeStyle = trunkcol;
+    ctx.fillStyle = leafcol;
+    ctx.lineWidth = stemThickness;
+    let width = height / 3.5;
+    ctx.rotate(angle * Math.PI / 180);
+    ctx.moveTo(0, raduis);
+    ctx.lineTo(0, raduis + height);
+    ctx.stroke();
+    ctx.lineTo(-width*0.25, raduis + height * 0.75);
+    ctx.lineTo(0, raduis + height * 0.75);
+    ctx.lineTo(-width*0.5, raduis + height * 0.5);
+    ctx.lineTo(0, raduis + height * 0.5);
+    ctx.lineTo(-width*0.75, raduis + height * 0.25);
+    ctx.lineTo(width * 0.75, raduis + height * 0.25);
+    ctx.lineTo(0, raduis + height * 0.5);
+    ctx.lineTo(width * 0.5, raduis + height * 0.5);
+    ctx.lineTo(0, raduis + height * 0.75);
+    ctx.lineTo(width * 0.25, raduis + height * 0.75);
+    ctx.lineTo(0, raduis + height);
+    ctx.rotate(-angle * Math.PI / 180);
+    ctx.fill();
+
 }
--- static/js/script.js
+++ static/js/script.js
@@ -7,19 +7,24 @@ const ScreenHeight = canvas.height = window.innerHeight;
 const ctx = canvas.getContext("2d");
 
 //ctx.fillRect(ScreenWidth-100, ScreenHeight-100,100,100);
-
-let world = new World(ScreenWidth, ScreenHeight, "grey");
+let treeCol = new treeCollection();
+//treeCol.add(Math.PI * 10, 100);
+//treeCol.add(1, 200);
+for (let i = 0; i < 80; i++){
+    treeCol.add(Math.random()*180, Math.random()*50+50);
+}
+let world = new World(ScreenWidth, ScreenHeight, "grey", treeCol);
 
 
 function animate(dt) {
     ctx.clearRect(0, 0, ScreenWidth, ScreenHeight);
-    //world.rotate((Math.PI / 180)*2);
-    world.angle += (Math.PI / 180) * 0.5;
+    world.rotate((Math.PI / 180)*0.5);
     world.draw(ctx);
     requestAnimationFrame(animate);
 }
 
 animate(0);
+
 
 
 
