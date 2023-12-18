f53dee8c641cced0c2e8fba3fe9117b9ce0e069b
dino-the-programer <dinop0928@gmail.com>
2023-12-18 12:08:59 +0530

    added the mountain and modified the lake generation with a lerp function

--- static/js/objects.js
+++ static/js/objects.js
@@ -7,20 +7,22 @@ class World{
         };
         this.style = style;
         this.props = props;
-        this.angle = Math.PI;
+        this.angle = 0;
     }
 
     draw(ctx) {
         ctx.save();
         ctx.translate(this.pos.x, this.pos.y);
         ctx.rotate(this.angle);
+        drawMountain(ctx,this.raduis,this.props[2].startangle,this.props[2].stopangle,this.props[2].heightarr,this.props[2].mountainCol);
         ctx.fillStyle = this.style;
         ctx.beginPath();
-        ctx.arc(0,0, this.raduis, 0, Math.PI * 2);
+        ctx.arc(0, 0, this.raduis, 0, 360 * Math.PI / 180);
         ctx.fill();
         this.props[0].array.forEach(tree => {
             drawTree(ctx, this.raduis, tree.angle, tree.height,"#234f42","#060f1f",4);
         });
+        drawLake(ctx,this.raduis,this.props[1].waterCol,this.props[1].startangle,this.props[1].stopangle,this.props[1].deptarray);
         ctx.restore();
     }
 
@@ -37,13 +39,37 @@ class treeCollection{
     }
 }
 
+class Lake{
+    constructor(startangle, stopangle, waterCol) {
+        this.startangle = startangle;
+        this.stopangle = stopangle;
+        this.waterCol = waterCol;
+        this.deptarray = [];
+    }
+    add(depth) {
+        this.deptarray.push(depth);
+    }
+}
+
+class Mountain{
+    constructor(startangle, stopangle, mountainCol) {
+        this.startangle = startangle;
+        this.stopangle = stopangle;
+        this.mountainCol = mountainCol;
+        this.heightarr = [];
+    }
+
+    add(height) {
+        this.heightarr.push(height);
+    }
+}
 
 function drawTree(ctx, raduis, angle, height,leafcol,trunkcol,stemThickness) {
     ctx.beginPath();
     ctx.strokeStyle = trunkcol;
     ctx.fillStyle = leafcol;
-    ctx.lineWidth = stemThickness;
     let width = height / 3.5;
+    ctx.lineWidth = (height/width)/2;
     ctx.rotate(angle * Math.PI / 180);
     ctx.moveTo(0, raduis);
     ctx.lineTo(0, raduis + height);
@@ -62,4 +88,39 @@ function drawTree(ctx, raduis, angle, height,leafcol,trunkcol,stemThickness) {
     ctx.rotate(-angle * Math.PI / 180);
     ctx.fill();
 
+}
+
+
+function drawLake(ctx, raduis, waterCol, startangle, stopangle, deptharray) {
+
+    ctx.beginPath();
+    ctx.fillStyle = waterCol;
+    ctx.rotate(startangle * Math.PI / 180);
+    ctx.moveTo(0, raduis);
+    ctx.rotate(-startangle * Math.PI / 180);
+    for (let i = 0; i < deptharray.length; i++){
+        ctx.rotate(lerp(startangle, stopangle, i / deptharray.length) * Math.PI / 180);
+        ctx.lineTo(0, raduis - deptharray[i]);
+        ctx.rotate(-lerp(startangle, stopangle, i / deptharray.length) * Math.PI / 180);
+    }
+    ctx.arc(0, 0, raduis, 0, -(stopangle-startangle) * Math.PI / 180,true);
+    ctx.fill();
+}
+
+function drawMountain(ctx,raduis,startangle,stopangle,heightarr,mountainCol) {
+    ctx.beginPath();
+        ctx.fillStyle = mountainCol;
+        ctx.strokeStyle = "brown";
+        ctx.rotate(startangle * Math.PI / 180);
+        ctx.moveTo(0, raduis + heightarr[0]);
+        ctx.rotate(-startangle * Math.PI / 180);
+        for (let i=1; i < heightarr.length; i++){
+            ctx.rotate(lerp(270,360,i/heightarr.length) * Math.PI / 180);
+            ctx.lineTo(0, raduis + heightarr[i]);
+            ctx.rotate(-lerp(270,360,i/heightarr.length) * Math.PI / 180);
+        }
+        ctx.fill();
+}
+function lerp(a, b, t) {
+    return a + (b - a) * t;
 }
--- static/js/script.js
+++ static/js/script.js
@@ -8,23 +8,46 @@ const ctx = canvas.getContext("2d");
 
 //ctx.fillRect(ScreenWidth-100, ScreenHeight-100,100,100);
 let treeCol = new treeCollection();
-//treeCol.add(Math.PI * 10, 100);
-//treeCol.add(1, 200);
 for (let i = 0; i < 80; i++){
-    treeCol.add(Math.random()*180, Math.random()*50+50);
+    let maxHeight = ScreenHeight*0.08;
+    let minHeight = ScreenHeight*0.05;
+    treeCol.add(Math.random()*90+90, Math.random()*maxHeight+minHeight);
 }
-let world = new World(ScreenWidth, ScreenHeight, "grey", treeCol);
+
+let lake = new Lake(185, 270, "lightblue");
+for (let i = 0; i < 15; i++){
+    lake.deptarray.push(Math.random() * 50 + 50);
+}
+lake.deptarray.push(0);
+
+
+let mountain = new Mountain(270, 360, "brown");
+let mountainRes = 20;
+mountain.heightarr.push(0);
+for (let i = 0; i < mountainRes; i++){
+    mountain.heightarr.push(Math.random() * 100)
+}
+mountain.heightarr.sort()
+let mountain2half = [];
+for (let i = 0; i < mountainRes; i++){
+    mountain2half.push(Math.random() *100)
+}
+mountain2half.sort();
+mountain2half.reverse();
+mountain.heightarr.push(...mountain2half);
+mountain.heightarr.push(0);
+
+let world = new World(ScreenWidth, ScreenHeight, "grey", treeCol, lake, mountain);
 
 
 function animate(dt) {
     ctx.clearRect(0, 0, ScreenWidth, ScreenHeight);
     world.rotate((Math.PI / 180)*0.5);
+    //world.angle = 225 * Math.PI / 180;
     world.draw(ctx);
     requestAnimationFrame(animate);
 }
 
 animate(0);
-
-
 
 
